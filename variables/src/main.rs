4a68c19fdfc34fd28f6f7850445a24c1a62188e1
SMG-717 <saifgaber6648@gmail.com>
2024-02-14 17:44:24 +0000

    Shadowing example

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,17 +1,16 @@
 use std::process::exit;
 
-const SEVEN: u32 = 7 * 2;
+const SEVEN: i32 = 7 * 2;
 
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6 * SEVEN;
-    println!("The value of x is: {x}");
-    x = 6 * number();
+    let x = 5;
+    {
+        let x = 6 * SEVEN;
+        println!("The value of inner x is: {x}");
+        
+        let x = "hello";
+        println!("The value of inner x is now: {x}");
+    }
     println!("The value of x is: {x}");
     exit(0)
-}
-
-fn number() -> u32 {
-    return 7 * 2;
 }
